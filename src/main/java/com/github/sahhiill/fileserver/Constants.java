9b8ffd9469077b778db9440ef82aabb750779b19
sahil <sahilpanchal2903@gmail.com>
2023-08-29 20:20:28 +0100

    Add Constants.java

--- src/main/java/com/github/sahhiill/fileserver/Constants.java
+++ src/main/java/com/github/sahhiill/fileserver/Constants.java
@@ -1,2 +1,8 @@
-package com.github.sahhiill.fileserver;public class Constants {
+package com.github.sahhiill.fileserver;
+
+import java.util.Optional;
+
+public class Constants {
+
+    public static Optional<String> ROOT_FOLDER = Optional.empty();
 }
